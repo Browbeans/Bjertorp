223757ee44d6f52bca5b41b72ff50a5b5450ef3a
Oliver Johansson <oliverjohansson3@gmail.com>
2021-08-23 22:08:31 +0200

    Started backend with users

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const cookieSession = require('cookie-session')
+const express = require('express')
+const mongoose = require('mongoose')
+const userRouter = require('./users/user.router')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+const uri = "mongodb+srv://Browbeans:Freak219@cluster0.whre4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose.connect(uri, options)
+.then((result) => app.listen(PORT))
+.catch((err) => {console.log(err);})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieSession({
+    name: "session", 
+    secret: "s3cr3tK3y",
+    secure: false,
+    maxAge: 1000*60,
+    httpOnly: true
+}))
+
+app.use(userRouter)
+
+app.get("/", (req, res) => {
+    console.log('Server Connected')
+})
+
