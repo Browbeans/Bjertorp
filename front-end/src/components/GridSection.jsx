db7d618917dcb5e40ad03894db24d90f172ae00b
Oliver Johansson <oliverjohansson3@gmail.com>
2021-08-25 10:19:07 +0200

    added grid-section to main

---
+++ front-end/src/components/GridSection.jsx
@@ -0,0 +1,14 @@
+import "../style/main.css"
+
+function GridSection() {
+    return(
+        <div className="grid-section">
+            <div className="news"></div>
+            <div className="court-status"></div>
+            <div className="booking"></div>
+            <div className="court-guide"></div>
+        </div>
+    )
+}
+
+export default GridSection
--- front-end/src/components/Main.jsx
+++ front-end/src/components/Main.jsx
@@ -1,9 +1,10 @@
-
+import "../style/main.css"
+import GridSection from "./GridSection"
 
 function Main() {
     return(
         <div className="main">
-
+            <GridSection />
         </div>
     )
 }
