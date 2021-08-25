5afea3499bd589a94aee218f7b26046bd7ca8664
Oliver Johansson <oliverjohansson3@gmail.com>
2021-08-25 10:48:49 +0200

    edited grid-section

--- front-end/src/components/GridSection.jsx
+++ front-end/src/components/GridSection.jsx
@@ -1,12 +1,29 @@
 import "../style/main.css"
 
 function GridSection() {
+
     return(
         <div className="grid-section">
-            <div className="news"></div>
-            <div className="court-status"></div>
-            <div className="booking"></div>
-            <div className="court-guide"></div>
+            <div className="news">
+                <h3>
+                    Nyheter
+                </h3>
+            </div>
+            <div className="court-status">
+                <h3>
+                    Banstatus
+                </h3>
+            </div>
+            <div className="booking">
+                <h3>
+                    Boka din tid här
+                </h3>
+            </div>
+            <div className="court-guide">
+                <h3>
+                    Banan
+                </h3>
+            </div>
         </div>
     )
 }
